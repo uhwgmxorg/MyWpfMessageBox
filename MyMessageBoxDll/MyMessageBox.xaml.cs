3be89e8022df3e4448cd8c6a4dd88eaf8c484e74
Uwe on APOS-NBW10-02 <uhw@gmx.org>
2016-09-22 20:55:33 +0200

    Start.

---
+++ MyMessageBoxDll/MyMessageBox.xaml.cs
@@ -0,0 +1,299 @@
+﻿using System.ComponentModel;
+using System.Windows;
+using System.Windows.Media;
+
+namespace MyMessageBoxDll
+{
+    /// <summary>
+    /// Interaktionslogik für MessageBoxA.xaml
+    /// </summary>
+    public partial class MyMessageBox : Window, INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public enum MessageBoxAColor
+        {
+            RED,
+            BLUE,
+            YELLOW,
+            GREEN,
+            GRAY
+        }
+
+        public const uint NOTHING       = 0x00000000;
+        public const uint MODAL         = 0x00000001;
+        public const uint CENTER_WINDOW = 0x00000002;
+        public const uint ACTIVATE      = 0x00000004;
+        public const uint TOPMOST       = 0x00000008;
+        public const uint CLOSABLE      = 0x00000010;
+        public const uint MOVABLE       = 0x00000020;
+        public const uint HIDEABLE      = 0x00000040;
+
+        private double _desktopWorkingAreaRight = 0.0;
+        private double _desktopWorkingAreaBottom = 0.0;
+        private bool _closable = false;
+        private bool _movable = false;
+        private bool _hideable = false;
+
+        /// <summary>
+        /// Properties
+        /// </summary>
+        private double transparents;
+        public double Transparents { get { return transparents; } set { transparents = value; OnPropertyChanged("Transparents"); } }
+        private string message { get; set; }
+        public string Message { get { return message; } set { message = value; OnPropertyChanged("Message"); } }
+
+        private Color color1;
+        public Color Color1 { get { return color1; } set { color1 = value; OnPropertyChanged("Color1"); } }
+        private Color color2;
+        public Color Color2 { get { return color2; } set { color2 = value; OnPropertyChanged("Color2"); } }
+
+        private Brush backgrount1;
+        public Brush Backgrount1 { get { return backgrount1; } set { backgrount1 = value; OnPropertyChanged("Backgrount1"); } }
+        private Brush backgrount2;
+        public Brush Backgrount2 { get { return backgrount2; } set { backgrount2 = value; OnPropertyChanged("Backgrount2"); } }
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        public MyMessageBox()
+        {
+            InitializeComponent();
+            DataContext = this;
+            Transparents = 1;
+            Hide();
+            SetMessageBoxAColor(MessageBoxAColor.RED);
+        }
+
+        /******************************/
+        /*      Public Functions      */
+        /******************************/
+        #region  Public Functions
+
+        /// <summary>
+        /// ShowMessageBoaA
+        /// </summary>
+        public void ShowMyMessageBox(string message,uint characteristics, MessageBoxAColor messageBoxAColor = MessageBoxAColor.RED)
+        {
+            SetMessageBoxAColor(messageBoxAColor);
+            Message = message;
+            Transparents = 1;
+            if ((CENTER_WINDOW & characteristics) != NOTHING) CenterWindow(this, _desktopWorkingAreaRight, _desktopWorkingAreaBottom);
+            if ((ACTIVATE & characteristics) != NOTHING) Activate();
+            if ((TOPMOST & characteristics) != NOTHING) Topmost = true; else Topmost = false;
+            if ((CLOSABLE & characteristics) != NOTHING) _closable = true; else _closable = false;
+            if ((MOVABLE & characteristics) != NOTHING) _movable = true; else _movable = false;
+            if ((HIDEABLE & characteristics) != NOTHING) _hideable = true; else _hideable = false;
+
+            if ((MODAL & characteristics) != NOTHING)
+            {
+                if(IsVisible)
+                    Hide();
+                ShowDialog();
+            }
+            else
+                Show();
+        }
+
+        /// <summary>
+        /// HideMessageBoaA
+        /// </summary>
+        public void HideMyMessageBox()
+        {
+            Hide();
+        }
+
+        #endregion
+        /******************************/
+        /*       Button Events        */
+        /******************************/
+        #region Button Events
+
+        /// <summary>
+        /// button_CloseClick
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void button_CloseClick(object sender, RoutedEventArgs e)
+        {
+            if (!_closable) { System.Console.Beep(); return; }
+            Hide();
+        }
+
+        #endregion
+        /******************************/
+        /*      Menu Events           */
+        /******************************/
+        #region Menu Events
+
+        #endregion
+        /******************************/
+        /*      Other Events          */
+        /******************************/
+        #region Other Events
+
+        /// <summary>
+        /// Window_Main_Loaded
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Window_Main_Loaded(object sender, RoutedEventArgs e)
+        {
+            var desktopWorkingArea = System.Windows.SystemParameters.WorkArea;
+
+            _desktopWorkingAreaRight = desktopWorkingArea.Right;
+            _desktopWorkingAreaBottom = desktopWorkingArea.Bottom;
+
+            CenterWindow(this,_desktopWorkingAreaRight,_desktopWorkingAreaBottom);
+
+            LoadStrings();
+        }
+
+        /// <summary>
+        /// Window_Main_LostFocus
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Window_Main_LostFocus(object sender, RoutedEventArgs e)
+        {
+        }
+
+        /// <summary>
+        /// Window_MouseLeftButtonDown
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>Window_MouseRightButtonDown
+        private void Window_MouseLeftButtonDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            if (!_movable) return;
+            DragMove();
+        }
+
+        /// <summary>
+        /// Window_MouseRightButtonDown
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Window_MouseRightButtonDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+        }
+
+        /// <summary>
+        /// Window_MouseWheel
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Window_MouseWheel(object sender, System.Windows.Input.MouseWheelEventArgs e)
+        {
+            if (!_hideable) return;
+            double d = (double)e.Delta / 1000;
+            double o = Transparents;
+
+            o -= d;
+            if (0.2 <= o && o <= 1)
+                Transparents = o;
+        }
+
+        #endregion
+        /******************************/
+        /*      Other Functions       */
+        /******************************/
+        #region Other Functions
+
+        /// <summary>
+        /// SetMessageBoxAColor
+        /// </summary>
+        /// <param name="messageBoxColor"></param>
+        private void SetMessageBoxAColor(MessageBoxAColor messageBoxColor)
+        {
+            switch (messageBoxColor)
+            {
+                case MessageBoxAColor.RED:
+                    Color1 = StringToColor("#FFFF3F3F");
+                    Color2 = StringToColor("#FFFF8A8A");
+                    break;
+                case MessageBoxAColor.BLUE:
+                    Color1 = StringToColor("#FF3F50FF");
+                    Color2 = StringToColor("#FF8A8AFF");
+                    break;
+                case MessageBoxAColor.YELLOW:
+                    Color1 = StringToColor("#FFEEFF3F");
+                    Color2 = StringToColor("#FFFAFF8A");
+                    break;
+                case MessageBoxAColor.GREEN:
+                    Color1 = StringToColor("#FF007E1D");
+                    Color2 = StringToColor("#FFAAFF8A");
+                    break;
+                case MessageBoxAColor.GRAY:
+                    Color1 = StringToColor("#FF9C9A9A");
+                    Color2 = StringToColor("#FFCFCFCF");
+                    break;
+            }
+            Backgrount1 = new SolidColorBrush(Color1);
+            Backgrount2 = new SolidColorBrush(Color2);
+        }
+
+        /// <summary>
+        /// StringToColor
+        /// </summary>
+        /// <param name="colorString"></param>
+        /// <returns></returns>
+        private Color StringToColor(string colorString)
+        {
+            Color c = new Color();
+            if (colorString.StartsWith("#"))
+            {
+                colorString = colorString.Replace("#", "");
+                byte a = System.Convert.ToByte("ff", 16);
+                byte pos = 0;
+                if (colorString.Length == 8)
+                {
+                    a = System.Convert.ToByte(colorString.Substring(pos, 2), 16);
+                    pos = 2;
+                }
+                byte r = System.Convert.ToByte(colorString.Substring(pos, 2), 16);
+                pos += 2;
+                byte g = System.Convert.ToByte(colorString.Substring(pos, 2), 16);
+                pos += 2;
+                byte b = System.Convert.ToByte(colorString.Substring(pos, 2), 16);
+                c = Color.FromArgb(a, r, g, b);
+                return c;
+            }
+
+            return c;
+        }
+
+        /// <summary>
+        /// CenterWindow
+        /// </summary>
+        /// <param name="win"></param>
+        /// <param name="desktopWorkingAreaRight"></param>
+        /// <param name="desktopWorkingAreaBottom"></param>
+        private void CenterWindow(Window win, double desktopWorkingAreaRight, double desktopWorkingAreaBottom)
+        {
+            if (win == null || desktopWorkingAreaRight < 0 || desktopWorkingAreaBottom < 0)
+                return;
+            win.Left = (desktopWorkingAreaRight / 2 + win.Width / 2) - win.Width;
+            win.Top = (desktopWorkingAreaBottom / 2 + win.Height / 2) - win.Height;
+        }
+
+        /// <summary>
+        /// LoadStrings
+        /// </summary>
+        private void LoadStrings()
+        {
+        }
+
+        /// <summary>
+        /// OnPropertyChanged
+        /// </summary>
+        /// <param name="p"></param>
+        private void OnPropertyChanged(string p)
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(p));
+        }
+
+        #endregion
+    }
+}
---
+++ MyWpfMessageBoxTestExe/MyWpfMessageBoxTestExe/MainWindow.xaml.cs
@@ -0,0 +1,224 @@
+﻿using MyMessageBoxDll;
+using System;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace MyWpfMessageBoxTestExe
+{
+    /// <summary>
+    /// Interaktionslogik für MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window, INotifyPropertyChanged
+    {
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        MyMessageBox _messageBox = new MyMessageBox();
+        uint _characteristics = 0;
+
+        private MyMessageBox.MessageBoxAColor mBoxColor = MyMessageBox.MessageBoxAColor.RED;
+        public MyMessageBox.MessageBoxAColor MBoxColor
+        {
+            get
+            {
+                return mBoxColor;
+            }
+            set
+            {
+                mBoxColor = value;
+                OnPropertyChanged("RED");
+                OnPropertyChanged("BLUE");
+                OnPropertyChanged("YELLOW");
+                OnPropertyChanged("GREEN");
+                OnPropertyChanged("GRAY");
+                UpdateMyMessageBox();
+            }
+        }
+        public bool IsRed
+        {
+            get { return MBoxColor == MyMessageBox.MessageBoxAColor.RED; }
+            set { MBoxColor = value ? MyMessageBox.MessageBoxAColor.RED : MBoxColor; }
+        }
+        public bool IsBlue
+        {
+            get { return MBoxColor == MyMessageBox.MessageBoxAColor.BLUE; }
+            set { MBoxColor = value ? MyMessageBox.MessageBoxAColor.BLUE : MBoxColor; }
+        }
+        public bool IsYellow
+        {
+            get { return MBoxColor == MyMessageBox.MessageBoxAColor.YELLOW; }
+            set { MBoxColor = value ? MyMessageBox.MessageBoxAColor.YELLOW : MBoxColor; }
+        }
+        public bool IsGreen
+        {
+            get { return MBoxColor == MyMessageBox.MessageBoxAColor.GREEN; }
+            set { MBoxColor = value ? MyMessageBox.MessageBoxAColor.GREEN : MBoxColor; }
+        }
+        public bool IsGray
+        {
+            get { return MBoxColor == MyMessageBox.MessageBoxAColor.GRAY; }
+            set { MBoxColor = value ? MyMessageBox.MessageBoxAColor.GRAY : MBoxColor; }
+        }
+
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = this;
+        }
+
+        /******************************/
+        /*       Button Events        */
+        /******************************/
+        #region Button Events
+        
+        /// <summary>
+        /// button_Show_Click
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void button_Show_Click(object sender, RoutedEventArgs e)
+        {
+            Debug.WriteLine(String.Format("{0} {1}", Dec2Bin(_characteristics, 16), MBoxColor));
+            _messageBox.ShowMyMessageBox("My very importen message !!", _characteristics, MBoxColor);
+        }
+
+        /// <summary>
+        /// button_Hide_Click
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void button_Hide_Click(object sender, RoutedEventArgs e)
+        {
+            _messageBox.HideMyMessageBox();
+        }
+
+        /// <summary>
+        /// button_Close_Click
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void button_Close_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        #endregion
+        /******************************/
+        /*      Menu Events           */
+        /******************************/
+        #region Menu Events
+
+        #endregion
+        /******************************/
+        /*      Other Events          */
+        /******************************/
+        #region Other Events
+
+        private void checkBox_Click1(object sender, RoutedEventArgs e)
+        {
+            if ((bool)((CheckBox)(sender)).IsChecked) _characteristics |= MyMessageBox.NOTHING; else _characteristics &= ~MyMessageBox.NOTHING;
+            Debug.WriteLine(String.Format("{0} {1}", Dec2Bin(_characteristics, 16), sender.ToString()));
+            UpdateMyMessageBox();
+        }
+        private void checkBox_Click2(object sender, RoutedEventArgs e)
+        {
+            if ((bool)((CheckBox)(sender)).IsChecked) _characteristics |= MyMessageBox.MODAL; else _characteristics &= ~MyMessageBox.MODAL;
+            Debug.WriteLine(String.Format("{0} {1}", Dec2Bin(_characteristics, 16), sender.ToString()));
+            UpdateMyMessageBox();
+        }
+        private void checkBox_Click3(object sender, RoutedEventArgs e)
+        {
+            if ((bool)((CheckBox)(sender)).IsChecked) _characteristics |= MyMessageBox.CENTER_WINDOW; else _characteristics &= ~MyMessageBox.CENTER_WINDOW;
+            Debug.WriteLine(String.Format("{0} {1}", Dec2Bin(_characteristics, 16), sender.ToString()));
+            UpdateMyMessageBox();
+        }
+        private void checkBox_Click4(object sender, RoutedEventArgs e)
+        {
+            if ((bool)((CheckBox)(sender)).IsChecked) _characteristics |= MyMessageBox.ACTIVATE; else _characteristics &= ~MyMessageBox.ACTIVATE;
+            Debug.WriteLine(String.Format("{0} {1}", Dec2Bin(_characteristics, 16), sender.ToString()));
+            UpdateMyMessageBox();
+        }
+        private void checkBox_Click5(object sender, RoutedEventArgs e)
+        {
+            if ((bool)((CheckBox)(sender)).IsChecked) _characteristics |= MyMessageBox.TOPMOST; else _characteristics &= ~MyMessageBox.TOPMOST;
+            Debug.WriteLine(String.Format("{0} {1}", Dec2Bin(_characteristics, 16), sender.ToString()));
+            UpdateMyMessageBox();
+        }
+        private void checkBox_Click6(object sender, RoutedEventArgs e)
+        {
+            if ((bool)((CheckBox)(sender)).IsChecked) _characteristics |= MyMessageBox.CLOSABLE; else _characteristics &= ~MyMessageBox.CLOSABLE;
+            Debug.WriteLine(String.Format("{0} {1}", Dec2Bin(_characteristics, 16), sender.ToString()));
+            UpdateMyMessageBox();
+        }
+        private void checkBox_Click7(object sender, RoutedEventArgs e)
+        {
+            if ((bool)((CheckBox)(sender)).IsChecked) _characteristics |= MyMessageBox.MOVABLE; else _characteristics &= ~MyMessageBox.MOVABLE;
+            Debug.WriteLine(String.Format("{0} {1}", Dec2Bin(_characteristics, 16), sender.ToString()));
+            UpdateMyMessageBox();
+        }
+        private void checkBox_Click8(object sender, RoutedEventArgs e)
+        {
+            if ((bool)((CheckBox)(sender)).IsChecked) _characteristics |= MyMessageBox.HIDEABLE; else _characteristics &= ~MyMessageBox.HIDEABLE;
+            Debug.WriteLine(String.Format("{0} {1}", Dec2Bin(_characteristics, 16), sender.ToString()));
+            UpdateMyMessageBox();
+        }
+
+        /// <summary>
+        /// Window_Closed
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Window_Closed(object sender, System.EventArgs e)
+        {
+            _messageBox.Close();
+        }
+
+        #endregion
+        /******************************/
+        /*      Other Functions       */
+        /******************************/
+        #region Other Functions
+
+        /// <summary>
+        /// UpdateMyMessageBox
+        /// </summary>
+        private void UpdateMyMessageBox()
+        {
+            Debug.WriteLine(String.Format("{0} {1}", Dec2Bin(_characteristics, 16), MBoxColor));
+            if (_messageBox.IsVisible)
+                _messageBox.ShowMyMessageBox("My very importen message !!", _characteristics, MBoxColor);
+        }
+
+        /// <summary>
+        /// Dec2Bin
+        /// </summary>
+        /// <param name="dezimalzahl"></param>
+        /// <param name="bitanzahl"></param>
+        /// <returns></returns>
+        public string Dec2Bin(long dezimalzahl, int bitanzahl)
+        {
+            System.Text.StringBuilder sb = new System.Text.StringBuilder();
+            for (int i = 0; i < bitanzahl; dezimalzahl = dezimalzahl >> 1, i++)
+                sb.Insert(0, dezimalzahl & 1);
+            return sb.ToString();
+        }
+
+        /// <summary>
+        /// OnPropertyChanged
+        /// </summary>
+        /// <param name="p"></param>
+        private void OnPropertyChanged(string p)
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(p));
+        }
+
+        #endregion
+    }
+}
